697fa00417bef4ec2b0afb5da8a6580c9c9c94e2
Antonio Rafael <andrade.rafa71@gmail.com>
2023-01-03 20:39:29 -0300

    ajustando a responsividade

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ let btnUP = document.querySelectorAll('.question .up')
 let down = document.querySelectorAll('.down')
 let img = document.createElement('img')
 
+
+
 btnUP.forEach((element, index)=>{
     element.addEventListener('click', ()=>{
-        if(down[index].style.display == 'none'){
-            down[index].style.display = 'flex'
+        if(down[index].style.display == 'flex'){
+            down[index].style.display = 'none'
             btnUP[index].querySelector('img').style.transform = 'rotateZ(90deg)'
         }else{
-            down[index].style.display = 'none'
+            down[index].style.display = 'flex'
             btnUP[index].querySelector('img').style.transform = 'rotateZ(-90deg)'
         }
-
+        
     })
     
 })
