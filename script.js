fdd817c0a294e6eac8b15522b57481e526067cbf
RafaelAF <andrade.rafa71@gmail.com>
2023-01-03 16:23:15 -0300

    Continuação do layout e javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+let btnUP = document.querySelectorAll('.question .up')
+let down = document.querySelectorAll('.down')
+let img = document.createElement('img')
+
+btnUP.forEach((element, index)=>{
+    element.addEventListener('click', ()=>{
+        if(down[index].style.display == 'none'){
+            down[index].style.display = 'flex'
+            btnUP[index].querySelector('img').style.transform = 'rotateZ(90deg)'
+        }else{
+            down[index].style.display = 'none'
+            btnUP[index].querySelector('img').style.transform = 'rotateZ(-90deg)'
+        }
+
+    })
+    
+})
