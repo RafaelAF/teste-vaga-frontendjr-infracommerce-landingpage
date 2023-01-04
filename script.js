6f3b1c941e49a8ce3d4c20ce76889ca13b784d3c
Antonio Rafael <andrade.rafa71@gmail.com>
2023-01-04 20:52:10 -0300

    Finalizando o projeto

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 let btnUP = document.querySelectorAll('.question .up')
 let down = document.querySelectorAll('.downContainer')
-let img = document.createElement('img')
+let img = document.querySelectorAll('p img')
 
 
 
 btnUP.forEach((element, index)=>{
     element.addEventListener('click', ()=>{
-        if(down[index].style.display != 'none'){
+        if(down[index].style.display == 'flex'){
             down[index].style.display = 'none';
-            console.log('N tem ')
+            img[index].style.transform = 'rotateZ(-90deg)';
+            
         }else{
             down[index].style.display = 'flex';
+            img[index].style.transform = 'rotateZ(90deg)';
         }
         
     })
